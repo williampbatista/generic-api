4e0525552c28282989418b8118f0c3d218230f49
William Batista <william.pessoa@vegasolutions.com.br>
2020-02-18 16:06:11 -0300

    implementação metodos mok

--- src/main/java/br/com/tarz/genericapi/contoller/CustomerController.java
+++ src/main/java/br/com/tarz/genericapi/contoller/CustomerController.java
@@ -1,10 +1,61 @@
 package br.com.tarz.genericapi.contoller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.tarz.genericapi.resource.CustomerResource;
+import br.com.tarz.genericapi.service.CustomerService;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/customer")
 public class CustomerController {
+
+	@Autowired
+	CustomerService service;
+
+	@GetMapping
+	public ResponseEntity<Page<CustomerResource>> get(Object filter, Pageable pageable) {
+		Page<CustomerResource> customers = service.findByFilter(filter, pageable);
+		return ResponseEntity.ok().body(customers);
+	}
+
+	@GetMapping(value="/{id}")
+	public ResponseEntity<CustomerResource> get(@PathVariable Integer id){
+		CustomerResource  resource = service.findOne(id).get();
+		return ResponseEntity.ok(resource);
+	}
+
+	@PostMapping
+	public ResponseEntity<CustomerResource> post(@Valid @RequestBody CustomerResource resource,
+			UriComponentsBuilder uriBuilder) {
+		resource.setId(1);
+		URI path = uriBuilder.path("/api/customer/{id}").buildAndExpand(resource.getId()).toUri();
+		return ResponseEntity.created(path).body(resource);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<CustomerResource> put(@RequestBody CustomerResource resource, @PathVariable Integer id) {
+		return ResponseEntity.ok().body(resource);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> delete(@PathVariable Integer id) {
+		return ResponseEntity.ok().build();
+	}
 
 }
--- src/main/java/br/com/tarz/genericapi/service/CustomerService.java
+++ src/main/java/br/com/tarz/genericapi/service/CustomerService.java
@@ -1,9 +1,12 @@
 package br.com.tarz.genericapi.service;
 
-import java.awt.print.Pageable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.tarz.genericapi.resource.CustomerResource;
@@ -11,16 +14,29 @@ import br.com.tarz.genericapi.resource.CustomerResource;
 @Service
 public class CustomerService {
 
+	public CustomerResource customerMok() {
+		return new CustomerResource(1, 2, "document", "personType", "name", "tradeName", "email");
+	}
+	
+	public List<CustomerResource> listCustomerMok(){
+		List<CustomerResource> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(customerMok());
+		}
+		return list;
+	}
+
 	public Iterable<CustomerResource> findAll() {
-		return null;
+		return listCustomerMok();
 	}
 
 	public Optional<CustomerResource> findOne(Integer id) {
-		return null;
+		return Optional.of(customerMok());
 	}
 
 	public Page<CustomerResource> findByFilter(Object filter, Pageable pageable) {
-		return null;
+		Page<CustomerResource> page = new PageImpl<>(listCustomerMok());
+		return page;
 	}
 
 	public CustomerResource save(CustomerResource resource) {
